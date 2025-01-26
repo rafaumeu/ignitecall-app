9da2ab10f206ee8424bfdad208b9916f144741b0
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-26 20:38:56 -0300

    feat(styles): migrate global styles to TypeScript and add @ignite-ui/react
    - Replace globals.css with globals.ts for better type safety
    - Add @ignite-ui/react dependency for UI components
    - Update related pages to reflect changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,22 @@
+import { getCssText } from '@ignite-ui/react'
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+        <style id="stitches">{getCssText()}</style>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Heading } from '@ignite-ui/react'
+
 export default function Home() {
-  return <h1>Home</h1>
+  return <Heading as="h1">Home</Heading>
 }
