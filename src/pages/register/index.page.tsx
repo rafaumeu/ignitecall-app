5818945e0a0a60e06e2d973d6db1b9b6407b61cb
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-27 07:08:20 -0300

    feat(pages/register): add form validation and error handling
    - Add form validation using Zod and React Hook Form
    - Create FormError component to display error messages
    - Update Form component to display error messages for username and full name fields
    - Add disabled state to Button component when form is submitting
    - Update styles to include error message color
    - Import necessary dependencies from Zod and React Hook Form

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,4 +1,4 @@
-import { Container, Form } from './style'
+import { Container, Form, FormError } from './style'
 import { Button, TextInput } from '@ignite-ui/react'
 import { MultiStep } from '@ignite-ui/react'
 import { Text } from '@ignite-ui/react'
@@ -6,8 +6,33 @@ import { Heading } from '@ignite-ui/react'
 
 import { Header } from '@/pages/register/style'
 import { ArrowRight } from 'phosphor-react'
-
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário deve ter no mínimo 3 caracteres' })
+    .regex(/^([a-z\\-])+$/i, {
+      message: 'O usuário deve ter apenas letras e hifens',
+    })
+    .transform(username => username.toLowerCase()),
+  name: z
+    .string()
+    .min(3, { message: 'O nome deve ter no mínimo 3 caracteres' }),
+})
+type RegisterFormData = z.infer<typeof registerFormSchema>
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+  async function handleRegister(data: RegisterFormData) {
+    console.log(data)
+  }
   return (
     <Container>
       <Header>
@@ -18,16 +43,26 @@ export default function Register() {
         </Text>
         <MultiStep size={4} currentStep={1} />
       </Header>
-      <Form as="form">
+      <Form as="form" onSubmit={handleSubmit(handleRegister)}>
         <label>
           <Text size="sm">Nome de usuário</Text>
-          <TextInput prefix="ignite.com/" placeholder={'seu-usuário'} />
+          <TextInput
+            prefix="ignite.com/"
+            placeholder={'seu-usuário'}
+            {...register('username')}
+          />
+          {errors.username && (
+            <FormError size="sm">{errors.username.message}</FormError>
+          )}
         </label>
         <label>
           <Text size="sm">Nome completo</Text>
-          <TextInput placeholder={'Seu nome'} />
+          <TextInput placeholder={'Seu nome'} {...register('name')} />
+          {errors.name && (
+            <FormError size="sm">{errors.name.message}</FormError>
+          )}
         </label>
-        <Button type="submit">
+        <Button type="submit" disabled={isSubmitting}>
           Próximo passo
           <ArrowRight />
         </Button>
