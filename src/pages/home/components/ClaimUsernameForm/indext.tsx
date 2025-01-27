5f7f6c897f37a7daad729c6ac82b3a43aee71f95
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-27 05:57:13 -0300

    feat(pages): add dependencies and update ClaimUsernameForm
    - Add @hookform/resolvers, react-hook-form and zod dependencies to package.json
    - Update src/pages/home/components/ClaimUsernameForm/indext.tsx to use new dependencies
    - Update yarn.lock to reflect new dependencies

--- src/pages/home/components/ClaimUsernameForm/indext.tsx
+++ src/pages/home/components/ClaimUsernameForm/indext.tsx
@@ -1,11 +1,26 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { Form } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+type ClaimUsernameFormSchema = z.infer<typeof claimUsernameFormSchema>
 export function ClaimUserNameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormSchema>()
+  async function handleClaimUsername(data: ClaimUsernameFormSchema) {
+    console.log(data)
+  }
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
