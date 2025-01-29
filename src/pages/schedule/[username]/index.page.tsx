589a50d522c92e7f009927e8e91d9a7158bb0be7
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-29 14:51:20 -0300

    feat(schedule): add schedule page and API endpoint
        - Create schedule page with user information and schedule form
        - Add API endpoint to retrieve user data for schedule page
        - Implement getStaticPaths and getStaticProps for schedule page
        - Improve code organization and formatting

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,55 @@
+import { Avatar, Text } from '@ignite-ui/react'
+import { Container, UserHeader } from './styles'
+import { Heading } from '@ignite-ui/react'
+import type { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '@/lib/prisma'
+import { ScheduleForm } from '@/pages/schedule/[username]/ScheduleForm'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+      <ScheduleForm />
+    </Container>
+  )
+}
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const userName = String(params?.username)
+  const user = await prisma.user.findUnique({
+    where: {
+      username: userName,
+    },
+  })
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: {
+      user: {
+        name: user.username,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
