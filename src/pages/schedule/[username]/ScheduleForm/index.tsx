00936cb771be66bad7ef91dc2969d5b7e71e948e
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-29 22:03:30 -0300

    feat(schedule): adds confirm step to schedule form
      - Adds confirm step to schedule form
      - Includes form fields for name, email, and observations
      - Includes buttons for cancel and confirm
      - Updates styles for confirm form and form actions
      - Adds new components to ScheduleForm

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,5 +1,6 @@
 import { CalendarStep } from '@/pages/schedule/[username]/ScheduleForm/CalendarStep'
+import { ConfirmStep } from '@/pages/schedule/[username]/ScheduleForm/ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  return <ConfirmStep />
 }
