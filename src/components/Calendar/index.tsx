1bd58e8395437ce2382988e6cc5191baad41b714
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-01-30 21:22:30 -0300

    feat(calendar): update calendar to display blocked days
    - Updates calendar to display blocked days based on user data
    - Adds error handling for update and query operations
    - Updates code to use new migrations
    - Removes unnecessary migrations
    - Updates code to use new migrations

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -99,7 +99,8 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map(date => {
--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -27,17 +27,29 @@ export default async function handle(
       user_id: user.id,
     },
   })
+  const padMonth = String(month).padStart(2, '0')
+
   const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter(weekDay => {
     return !availableWeekDays.some(
       availableWeekDay => availableWeekDay.week_day === weekDay
     )
   })
-  const blockedDatesRaw = await prisma.$queryRaw`
-    SELECT *
-    FROM schedulings S
-
-    WHERE S.user_id = ${user.id}
-      AND DATE_FORMAT(S.date, "%Y-%m" = ${`${year}-${month}`})
-  `
-  return res.json({ blockedWeekDays, blockedDatesRaw })
+  const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
+  SELECT 
+    EXTRACT(DAY FROM S.date) AS date,
+    COUNT(S.date) AS amount,
+    ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
+  FROM schedulings S
+  LEFT JOIN user_time_intervals UTI
+    ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
+      
+  WHERE S.user_id = ${user.id}
+    AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${padMonth}`}
+  GROUP BY EXTRACT(DAY FROM S.date),
+  ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+  HAVING amount >= size       
+    
+`
+  const blockedDates = blockedDatesRaw.map(item => Number(item.date))
+  return res.json({ blockedWeekDays, blockedDates })
 }
