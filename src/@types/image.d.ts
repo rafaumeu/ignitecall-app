14253c36374c1d879b44a31c50f18d16ec431498
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-02-17 14:40:11 -0300

    fix: update image type declarations with src property

--- src/@types/image.d.ts
+++ src/@types/image.d.ts
@@ -1,19 +1,35 @@
 declare module '*.png' {
-  const content: string
-  export default content
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+  };
+  export default content;
 }
 
 declare module '*.jpg' {
-  const content: string
-  export default content
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+  };
+  export default content;
 }
 
 declare module '*.jpeg' {
-  const content: string
-  export default content
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+  };
+  export default content;
 }
 
 declare module '*.svg' {
-  const content: string
-  export default content
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+  };
+  export default content;
 } 
