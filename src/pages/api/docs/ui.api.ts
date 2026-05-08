b72ded366961f78587a8410c6346c5555cd8b67e
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-05-07 21:22:32 -0300

    feat: add OpenAPI spec with interactive docs (#98)
    * feat: add OpenAPI spec with Swagger UI
    
    * fix: remove package-lock.json, use yarn.lock only

---
+++ src/pages/api/docs/ui.api.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const HTML = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Ignite Call API Docs</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+  <style>
+    html, body { margin: 0; padding: 0; }
+  </style>
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+  <script>
+    SwaggerUIBundle({
+      url: "/api/docs/json",
+      dom_id: '#swagger-ui',
+      presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset
+      ],
+      layout: "BaseLayout"
+    });
+  </script>
+</body>
+</html>`
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  res.status(200).send(HTML)
+}
